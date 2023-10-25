bc1b1210abbde4ad0eac659d598f18603d1d834c
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-10-25 15:35:29 +0200

    versione davvero davvero definitiva con numero generato casualmente

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const inserisciNome = prompt ("Inserisci il tuo nome", "Mario");
 const inserisciCognome = prompt ("inserisci il tuo cognome", "Rossi");
 const inserisciColorePreferito = prompt ("Inserisci Colore Preferito", "Magenta");
-const password = inserisciNome + inserisciCognome + inserisciColorePreferito + "#39";
+let randomNumber = Math.floor (Math.random ()*100)
+const password = inserisciNome + inserisciCognome + inserisciColorePreferito + randomNumber;
 
 document.getElementById("Titolo").innerHTML = "Complimenti, " + inserisciNome + "!";
 document.getElementById("Password").innerHTML = "La tua Password è :" + password;
