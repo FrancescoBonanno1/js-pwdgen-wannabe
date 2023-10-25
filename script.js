7e294346946a19097efc3c4c2a7631fbe8c5ae3e
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-10-25 15:24:20 +0200

    Versione definitiva

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const inserisciCognome = prompt ("inserisci il tuo cognome", "Rossi");
 const inserisciColorePreferito = prompt ("Inserisci Colore Preferito", "Magenta");
 const password = inserisciNome + inserisciCognome + inserisciColorePreferito + "#39";
 
-document.getElementById("Titolo").innerHTML = "Complimenti, " + inserisciNome;
+document.getElementById("Titolo").innerHTML = "Complimenti, " + inserisciNome + "!";
 document.getElementById("Password").innerHTML = "La tua Password è :" + password;
