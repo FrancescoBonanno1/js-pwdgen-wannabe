4bea2731f171e9d029a35754a53a1f952a29fcb1
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-10-25 15:21:01 +0200

    cambio di testo prompt

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const inserisciNome = prompt ("Inserisci il tuo nome", "Gennarino");
-const inserisciCognome = prompt ("inserisci il tuo cognome", "Gambocchione");
-const inserisciColorePreferito = prompt ("Inserisci Colore Preferito", "azzurro");
-const password = inserisciNome+ inserisciCognome + inserisciColorePreferito + "#39";
+const inserisciNome = prompt ("Inserisci il tuo nome", "Mario");
+const inserisciCognome = prompt ("inserisci il tuo cognome", "Rossi");
+const inserisciColorePreferito = prompt ("Inserisci Colore Preferito", "Magenta");
+const password = inserisciNome + inserisciCognome + inserisciColorePreferito + "#39";
 
 document.getElementById("Titolo").innerHTML = "Complimenti, " + inserisciNome;
 document.getElementById("Password").innerHTML = "La tua Password è :" + password;
